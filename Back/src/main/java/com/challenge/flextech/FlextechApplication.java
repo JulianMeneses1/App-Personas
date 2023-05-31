c18ac18e33abed11ea8944aff19058d455928f9d
JulianMeneses1 <julian.meneses11@gmail.com>
2023-05-31 09:06:34 -0300

    corregida url base swagger

--- Back/src/main/java/com/challenge/flextech/FlextechApplication.java
+++ Back/src/main/java/com/challenge/flextech/FlextechApplication.java
@@ -1,9 +1,22 @@
 package com.challenge.flextech;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition (
+        info = @Info(
+            title="App Personas",
+            version = "1.0.0",
+            description = "App para gestionar personas"             
+        ),
+        servers = {
+        @Server(url = "/", description = "Default Server URL")
+        }
+)
 public class FlextechApplication {
 
 	public static void main(String[] args) {
